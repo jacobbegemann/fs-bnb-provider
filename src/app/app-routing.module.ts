945f404170725eea4a91eade0877d0945adeb8bd
Jacob Begemann <jacob_begemann@brown.edu>
2019-05-30 01:30:14 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule'},
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule'},
+  { path: 'rental', loadChildren: './rental/rental.module#RentalPageModule'},
+  { path: 'new', loadChildren: './new/new.module#NewPageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private navctrl: NavController) {}
+
+  newListing() {
+    this.navctrl.navigateForward('new');
+  }
+
+  goToDetails() {
+    this.navctrl.navigateForward('rental');
+  }
 
 }
